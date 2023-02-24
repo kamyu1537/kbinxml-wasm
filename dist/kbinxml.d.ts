8d1805bdded43be5dbceb52b81dae9efcfb21779
Byeon Seongun <kamyu1537@gmail.com>
2023-02-25 02:15:47 +0900

    EncodingType enum 추가

--- dist/kbinxml.d.ts
+++ dist/kbinxml.d.ts
@@ -19,12 +19,21 @@ export function to_xml(data: Uint8Array): ResultType;
 
 export type ResultType = {
     data: string,
-    encoding: number,
+    encoding: EncodingType,
 };
 
 export type ToBinOptionType = {
     compression?: boolean,
-    encoding?: number,
+    encoding?: EncodingType,
 };
+
+export enum EncodingType {
+    None = 0x00,
+    ASCII = 0x20,
+    ISO_8859_1 = 0x40,
+    EUC_JP = 0x60,
+    SHIFT_JIS = 0x80,
+    UTF_8 = 0xA0,
+}
 
 
